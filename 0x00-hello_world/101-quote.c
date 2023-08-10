03e25b169d1bb5323f394c750a66d2ac4ede0ef6
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-10 16:36:16 -0700

    alx

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descreption: print with using fprint function
+ *
+ * Return: 1 (success)
+*/
+
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+	return (1)
+}
