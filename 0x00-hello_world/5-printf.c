e6c35111f9710901159fb6da82562a731f4b30d4
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-10 14:46:26 -0700

    alx

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,14 @@
 
 /**
  * main - Entry point
+ *
  * Describtion : This is a C program that prints with printf function
  *
  * return : always 0 (success)
 */
 
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n";
-	return (0); }
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
