8ff92a1cc91e29a5debff734cd65cd6596931c3c
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-10 14:40:04 -0700

    alx

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Describtion : This is a C program that prints with printf function
+ *
+ * return : always 0 (success)
+*/
+
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n";
+	return (0); }
