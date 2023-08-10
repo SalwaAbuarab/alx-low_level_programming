a5fef078133d84262ce855d96711ee5c98cdb7c8
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-10 13:51:18 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
-#!/bin/bash
-#inculde <stdio.h>
-int main()
-	puts("Programming is like building a multilingual puzzle);
-	return 0;
+#inculde < stdio.h >
+/**
+ * main - Entry point
+ *
+ * Descrebtion: A C program that prints with put function
+ * Return: alwayes 0 (success)
+*/
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0); }
