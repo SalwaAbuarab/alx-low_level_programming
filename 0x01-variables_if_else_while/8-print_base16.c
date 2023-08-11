f8a64966f7ee03a6a4de8c6987e204a6791f4144
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-11 16:09:18 -0700

    alx

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,18 +10,18 @@
 
 int main(void)
 {
-        int digit = 48; /*48: decimel rep of 0*/
+	int digit = 48;	/*48: decimel rep of 0*/
 
-        while (digit <= 102) /*102: decimel of rep f*/
-        {
-                putchar(digit);
+	while (digit <= 102) /*102: decimel of rep f*/
+	{
+		putchar(digit);
 
 		/* after 9 we jump till 96; '*/
 		if (digit == 57)
-			digit +=39;
+			digit += 39;
 		digit++;
-        }
-        putchar('\n');
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
