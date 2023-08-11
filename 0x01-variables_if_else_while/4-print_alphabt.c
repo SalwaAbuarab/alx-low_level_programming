09139b31afc7fd0695af0a4c2b4e17c45da2f06b
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-11 14:42:08 -0700

    alx

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Descreption:  prints the alphabet in lowercase except q and e
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'q' || ch == 'e')
+			ch++;
+
+
+		putchar(ch);
+		ch++;
+		putchar ('\n');
+	}
+	return (0);
+}
+
