6bc409c1c13b65b056f786153b6500a5e6faaaff
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-11 14:54:11 -0700

    alx

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Enrty point
+ *
+ * Descreption: prints all single digit numbers of base 10 starting from 0
+ *
+ *Return: 0 (success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		printf("d", nunm);
+	}
+	printf('\n');
+
+	return (0);
+}
