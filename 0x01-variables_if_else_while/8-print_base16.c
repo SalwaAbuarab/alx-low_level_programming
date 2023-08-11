039b59fcb4299fae0357d4733b6957576be54cd8
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-11 15:40:46 -0700

    alx

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descreption: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+        int digit = 48; /*48: decimel rep of 0*/
+
+        while (digit <= 120) /*102: decimel of rep f*/
+        {
+                putchar(digit);
+
+		/* after 9 we jump till 96; '*/
+		if (digit == 57)
+			digit +=39;
+		digit++;
+        }
+        putchar('\n');
+
+        return (0);
+}
