d22413abaa4b5f28d6211936f30c504a7c979647
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-15 10:48:50 -0700

    alx

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+void print_alphabet(void);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+}
