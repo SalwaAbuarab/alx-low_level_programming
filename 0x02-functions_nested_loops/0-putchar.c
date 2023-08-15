42447442e1553f309c322932ac3623c85b568908
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-15 10:20:12 -0700

    alx

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,9 +11,10 @@
 int main(void)
 
 {
-	char str[]:"_putchar";
+	char str[] : "_putchar";
 	int ch;
-	for(ch = 0; ch < 8; ch++)
+
+	for (ch = 0; ch < 8; ch++)
 	{
 		putchar(str(ch));
 	putchar("\n");
