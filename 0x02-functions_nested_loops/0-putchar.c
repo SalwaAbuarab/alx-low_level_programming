d38cc362b62cc8e31625e21f8a83c7714db8e4a7
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-15 10:16:36 -0700

    alx

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main"
+
+/**
+ * main - Entry point
+ *
+ * Description: that prints _putchar using command putchar
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+
+{
+	char str[]:"_putchar";
+	int ch;
+	for(ch = 0; ch < 8; ch++)
+	{
+		putchar(str(ch));
+	putchar("\n");
+	}
+	return (0);
+}
