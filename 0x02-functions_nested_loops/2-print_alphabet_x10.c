bf6f318bf8430170ec2161e4f4fc1fba36348097
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-15 11:14:56 -0700

    alx

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#includ "main.h"
+
+/**
+ * print_alphabet_x10 - function print alphabet x10 to prints 10 times
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int i, alphabet;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			putchar(alphabet);
+		}
+		putchar('\n')
+	}
+}
