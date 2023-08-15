a5c1ae4588ff6083ccd8229f714ddf00f6a82fa8
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-15 15:46:34 -0700

    alx

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers
+ *
+ * @int: te type of the input is integer
+ *
+ * Return: (sum)
+*/
+
+int add(int, int)
+{
+	int a, b;
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
