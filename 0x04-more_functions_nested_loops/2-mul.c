aa4282a9d4f736af39aaacddf56f3ff101a0c2db
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-16 23:22:12 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers
+ *
+ * @a: takes integer type input for function
+ *
+ * @b: takes integer type input for function
+ *
+ * Return: sum
+*/
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+
+	return (sum);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	if (num >= 48 && num <= 57)
+		_putchar(num);
+	_putchar('\n');
+}
