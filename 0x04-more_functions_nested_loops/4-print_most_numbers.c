7b9f0ac0c3709bf23995b3dd9eae8d25c08ab0fe
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-17 15:01:42 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  function that prints the numbers, from 0 to 9
+ *
+ * Return: alwayes o (sccess)
+*/
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	do {
+		_putchar(n + 48);
+		n++;
+	} while (n >= 2 && n <= 9);
+	_putchar('\n');
+}
