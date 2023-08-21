54f3e697765c876076a07825baf4d84b14c029c9
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-21 06:50:29 -0700

    alx

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  function that takes a pointer as parameter and
+ *                     updates the value points to 98
+ *
+ * @n: integer parameter
+ *
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
