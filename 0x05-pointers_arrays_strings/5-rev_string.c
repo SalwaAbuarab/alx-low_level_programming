acacb20a35e1aba4bbd477bf4b78b7cc508f4726
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-21 10:17:03 -0700

    alx

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string.
+ *
+ * @s: a pointer to string to point
+ *
+ * Return: noting
+*/
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+}
