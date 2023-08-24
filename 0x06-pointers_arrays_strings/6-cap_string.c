064fd69dc5eaf7022eeb1641ed1bcc27134baaee
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-24 15:32:47 -0700

    alx

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,65 @@
 #include "main.h"
+#include<stdio.h>
+
+/**
+ * islower - determines whether ascii is lowercase
+ *
+ * @c: character
+ *
+ * Return: 1 if true, 0 if false
+*/
+
+int islower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - determines whther ascii is delimiter
+ *
+ * @c: character
+ *
+ * Return: 1 if true, 0 if false
+ *
+*/
+
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = {" \t\n,.!?\"(){}"};
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+
+
 
 /**
  * *cap_string - function that capitalizes all words of a string
  *
- * @str: a return string
+ * @s: a return string
  *
  * Return: str string
 */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	char str[] = {0};
-	char delimiter[] = {" \t\n,.!?\"(){}"};
-	int i;
+	char *ptr  = s;
+	char foundDelimite = 1;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*s)
 	{
-		if (i == 0)
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (islower(*s) && foudDelimit)
 		{
-			if ((str[i] >= 'a' && str[i] <= 'z'))
-				str[i] = str[i] - 32;
-			continue;
-		}
-		for (i = 0; i < 12; i++)
-			if (str == delimiter[i])
-				return (1);
-		return (0);
-		{
-			if ((str[i] >= 'a' && str[i] <= 'z'))
-				str[i] = str[i] - 32;
-			continue;
+			*s -= 32;
+			foundDelimit = 0;
 		}
 		else
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
+			foundDelimit = 0;
+		s++;
 	}
-	return (str);
+	return (ptr);
 }
