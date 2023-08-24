2551d4d16b79f664a9fb14db01ba679d997d39a8
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-23 19:58:31 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncpy - function that copies a string
+ *
+ * @dest: pointer to destenation input
+ *
+ * @src: pointer to destenation input
+ *
+ * @n: int input
+ *
+ * Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++
+	}
+	return (dest);
+}
