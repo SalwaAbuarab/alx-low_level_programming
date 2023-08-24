708ddd8d6dff6a4b1ffc0087d1496be5ae3d85d0
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-24 09:56:19 -0700

    alx

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,7 +14,9 @@ void reverse_array(int *a, int n)
 {
 	int i, j, t;
 
-	for (i = 0, j = (n - 1); i < j; i++, i--)
+	for (i = 0; a[i] != '\0'; i++)
+		;
+	for (j = (n - 1); i < j; j--) 
 	{
 		t = a[i];
 		a[i] = a[j];
