4039e228a4e8b50165f39e6a7d7632140368aadd
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-08-24 10:35:43 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper -  function that changes all lowercase
+ * letters of a string to uppercase
+ *
+ * @str: string to return
+ *
+ * Return: str
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		for (str[i] >= 97 & str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	return (str)
+}
