3f975e23c68acfe1d4b75afcf42f79bf3f902914
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-09-05 13:15:11 -0700

    alx

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -36,8 +36,11 @@ int **alloc_grid(int width, int height)
 		free(ar);
 		return (NULL);
 	}
-	for (j = 0; j < width; j++)
-		ar[i][j] = 0;
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			ar[i][j] = 0;
+	}
 	return (ar);
 
 }
