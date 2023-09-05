d03e7d96076ff617efb55a701d703d6e559f640e
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-09-04 22:34:41 -0700

    alx

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,5 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+#include <stdlib.h>
+ar *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 #endif
