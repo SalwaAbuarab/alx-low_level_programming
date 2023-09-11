6551c285963b3e3064cc135d800ea12631de4efc
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-09-11 12:48:21 -0700

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef _3_FUNCTION_LIE_MACRO_H_
 #define _3_FUNCTION_LIE_MACRO_H_
 
-#define ABS ((x < 0 ? (-x) : (x))
+#define ABS ((x < 0) ? -(x) : (x))
 typedef int make_iso_compilers_happy;
 		
 #endif
