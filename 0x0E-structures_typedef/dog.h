6e33f75bf9501b94ac4f416e92f19981d483ed82
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-09-11 15:17:43 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - decribe the dog boy or girl
+ *
+ * @name: name of dog
+ * @age: how old is that dog?
+ * @owner: who is the lucky person that gets to live with that dog?
+ *
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H */
