3cde8c8b1ff28b9b1b12300cfde47b682b72d94f
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-09-11 17:46:19 -0700

    struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog -  function that frees dogs
+ *
+ * @d: pointer of struct dog
+ *
+ * Return: nothing
+ *
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
