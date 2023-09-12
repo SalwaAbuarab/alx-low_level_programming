3b8ee857eb170727431ae9f2fea7b9731d67870b
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-09-11 17:35:02 -0700

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,40 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ *
+ * @str: The string to copy
+ *
+ * Return: a pointer to the duplicated string, NULL if insufficient memory
+ * or if @str is NULL
+ *
+*/
+
+char *_strdup(char *str)
+{
+	char *arr;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	arr = malloc(sizeof(char) * (i + 1));
+
+	if (arr == NULL)
+		return (NULL);
+	while (str[j])
+	{
+		arr[j] = str[j];
+		j++;
+	}
+	arr[j] = 0;
+	return (arr);
+}
 
 /**
  * new_dog - function that creates a new dog
@@ -14,4 +49,32 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new;
+	char *ncpy;
+	char *ocpy;
+
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
+
+	ncpy = _strdup(name);
+	if (!ncpy && name)
+	{
+		free(new);
+		return (NULL);
+	}
+	ocpy = _strdup(owner);
+	if (!ocpy && owner)
+	{
+		free(ncpy);
+		free(new);
+		return (NULL);
+	}
+
+	new->name = ncpy;
+	new->age = age;
+	new->owner = ocpy;
+
+	return (new);
+
 }
