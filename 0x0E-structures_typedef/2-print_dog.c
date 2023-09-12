0184ca9639bdbf194005d20c9ba3a4638a9f8527
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-09-11 21:12:32 -0700

    struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,14 +18,13 @@ void print_dog(struct dog *d)
 		if (d->name == NULL || d->owner == NULL)
 		{
 			printf("(nil)");
-			printf("Age: %f\n", d->age);
 		}
 		else
 		{
 			printf("Name: %s\n", d->name);
-			printf("Age: %f\n", d->age);
 			printf("Owner: %s\n", d->owner);
 		}
+		printf("Age: %f\n", d->age);
 	}
 	else
 		return;
