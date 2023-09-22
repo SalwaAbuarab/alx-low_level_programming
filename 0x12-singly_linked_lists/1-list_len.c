f0b080124cf777520086c557b33bc8d7b4c9ff68
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-09-21 19:53:55 -0700

    lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * list_len - function that returns the number of elements in a linked list_t list
+ *
+ * @h: string to list
+ *
+ * Return:  returns the number of elements in a linked list_t list8
+ *
+*/
+size_t list_len(const list_t *h)
+{
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * struct list_s - singly linked list
@@ -21,5 +22,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 int _putchar(char c);
+size_t list_len(const list_t *h);
 
 #endif
