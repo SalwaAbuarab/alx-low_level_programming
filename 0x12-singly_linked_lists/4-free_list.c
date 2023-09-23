86ddd00adb551b2ff13dd2dfe1996da43e5569e3
SalwaAbuarab <salwa.abuarab@feps.edu.eg>
2023-09-22 17:28:22 -0700

    lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list -  function that frees a list_t list
+ *
+ * @head: head of the list
+ *
+ * Return :nothing
+ *
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
